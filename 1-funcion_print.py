6537cbf9feb805ea07f55cee5c541cb09d6c31e9
marian3299 <mariana.ag3299@gmail.com>
2024-09-26 20:30:09 -0600

    initial commit

---
+++ 1-funcion_print.py
@@ -0,0 +1,21 @@
+#print -> sirve para imprimir en consola
+print("Hola mundo")
+
+#Caracteres de escape \
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+#Multiples argumentos
+print("hola", "soy", "mariana")
+
+#Argumentos de palabra clave
+#end
+print("Mi nombre es", "Python", end=" ---")
+print("Monty python")
+
+#sep
+print("Mi nombre es", "Python", "Monty python", sep="-")
+
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
---
+++ 2-literales.py
@@ -0,0 +1,23 @@
+#Enteros, python acepta dos formas de escribirlos
+111111
+11_11_11
+
+#Octales 
+print(0o123)
+print(0O123)
+
+#Hexadecimales
+print(0x123)
+print(0X123)
+
+#Flotantes
+0.4
+2.5
+.4
+4.0
+
+#Exponentes -> los interpreta como flotantes
+print(3e8) #3x10 a la 8 (notacion cientifica)
+print(0.0000000000000000000001)
+
+#Cadenas
